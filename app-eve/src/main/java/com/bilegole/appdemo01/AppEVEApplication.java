eb21156f754abc5e9ea44385288604cf4ded70fc
yuang <958102068@qq.com>
2023-06-17 10:26:35 +0800

    重命名项目为app-eve

--- app-eve/src/main/java/com/bilegole/appdemo01/AppEVEApplication.java
+++ app-eve/src/main/java/com/bilegole/appdemo01/AppEVEApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppDemo01Application {
+public class AppEVEApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppDemo01Application.class, args);
+		SpringApplication.run(AppEVEApplication.class, args);
 	}
 
 }
